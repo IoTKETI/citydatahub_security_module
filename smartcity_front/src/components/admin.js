75e407919b7472cfbb1c16c3980fbd1065ac0337
simonieee <75366051+simonieee@users.noreply.github.com>
2023-01-09 13:02:22 +0900

    [UPDATE] title && button update

--- smartcity_front/src/components/admin.js
+++ smartcity_front/src/components/admin.js
@@ -1,20 +1,20 @@
-import React, { Component,Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 
-import { Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import '../stylesheets/login.css';
-import '../stylesheets/dhsec.css';
-import { PROGRAM_TYPE } from '../config';
+import "../stylesheets/login.css";
+import "../stylesheets/dhsec.css";
+import { PROGRAM_TYPE } from "../config";
 
 // function admin(props){
-    class admin extends Component{
-        render(){
-            const programType = PROGRAM_TYPE;
-            const callURL = programType === 'admin' ? '/accounts/sysUsers' : '/accounts/Users';
-        return(
-            <Fragment>
-                
-                 {/* <div className="index__button">
+class admin extends Component {
+  render() {
+    const programType = PROGRAM_TYPE;
+    const callURL =
+      programType === "admin" ? "/accounts/sysUsers" : "/accounts/Users";
+    return (
+      <Fragment>
+        {/* <div className="index__button">
                      <a className="button button__outline--primary" href=" ">관리자 계정 관리</a>
                  </div>
                  <div className="index__button">
@@ -28,26 +28,31 @@ import { PROGRAM_TYPE } from '../config';
                  </div>
                               
                 <button className="button__index" >일반 어플리케이션 관리</button> */}
-                <Link to ={callURL}>
-                <button className="button__index" >사용자 계정 관리</button>  
-                </Link>
-                {/* <Link to='/accounts/Users'>
+        <Link to={callURL}>
+          {programType === "admin" ? (
+            <button className="button__index">관리자 계정 관리</button>
+          ) : (
+            <button className="button__index">사용자 계정 관리</button>
+          )}
+        </Link>
+        {/* <Link to='/accounts/Users'>
                 <button className="button__index" >일반 사용자 계정 관리</button>
                 </Link>  */}
 
-                <Link to='/accounts/roles'>
-                <button className="button__index" >권한정보 관리</button>
-                </Link>
-                
-                <Link to='/apps/sysClients'>
-                <button className="button__index" >데이터 허브 어플리케이션 관리</button>    
-                </Link>
-                
-            </Fragment>
-        );
-        }
-    }
-    
+        <Link to="/accounts/roles">
+          <button className="button__index">권한정보 관리</button>
+        </Link>
+
+        <Link to="/apps/sysClients">
+          <button className="button__index">
+            데이터 허브 어플리케이션 관리
+          </button>
+        </Link>
+      </Fragment>
+    );
+  }
+}
+
 // }
 
 export default admin;
